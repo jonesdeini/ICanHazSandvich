b8d7d62a54442d464810bef44cc0e8f7d5dc6273
jonesdeini <jonesdeini@gmail.com>
2012-08-20 23:49:11 -0400

    start of test stuff

---
+++ test/minitest_helper.rb
@@ -0,0 +1,9 @@
+ENV["RAILS_ENV"] = "test"
+require File.expand_path('../../config/environment', __FILE__)
+
+require "minitest/autorun"
+require "minitest/rails"
+require "minitest/rails/capybara"
+require "minitest/pride"
+
+MiniTest::Rails.override_testunit!
