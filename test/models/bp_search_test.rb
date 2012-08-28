dfc91cd72de34e67601d62bf63e2747c74b1dd1d
jonesdeini <jonesdeini@gmail.com>
2012-08-28 12:16:30 -0400

    oops

---
+++ test/models/bp_search_test.rb
@@ -0,0 +1,18 @@
+require 'minitest_helper'
+
+describe BP_Search do
+
+  before do
+    #bp = 
+    @steam_id = "76561198015466913"
+    @s_f_knife = create :strange_festive_knife
+  end
+
+  it "must work" do
+    puts BaseItem.count
+  end
+
+  it "must still work" do
+    puts BaseItem.count
+  end
+end
