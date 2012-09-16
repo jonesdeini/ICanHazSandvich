b5da5618f6a76d07dc2591fab53716a934d285fc
jonesdeini <jonesdeini@gmail.com>
2012-09-16 10:04:50 -0400

    lotus search works, takes around 5 min. still need a count or index

--- app/models/steam_id_scraper.rb
+++ app/models/steam_id_scraper.rb
@@ -9,7 +9,7 @@ class SteamIdScraper
       hydra.queue(playerinfo_page = Typhoeus::Request.new(playerinfo_id_url))
       playerinfo_page.on_complete do |response|
         if response.success?
-          steam_id = response.body.scan(/http\:\/\/steamcommunity\.com\/profiles\/(\d+)/)
+          steam_id = response.body.scan(/http\:\/\/steamcommunity\.com\/profiles\/(\d+)/).flatten.first
           BackpackRetriever.new(hydra, 0, steam_id) if steam_id
         else
           puts "player info page request failed"
