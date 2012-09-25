ef6c1d6ab2133e3f44833d309c65bf1d48c789ba
jonesdeini <jonesdeini@gmail.com>
2012-09-24 23:15:38 -0400

    start of sorting

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,10 @@ class ItemsController < ApplicationController
 
   def show
     @item = BaseItem.find params[:id]
+    #@players = @item.players.sort{ |x,y| y.updated_at <=> x.updated_at }
+
+    @players = @item.players.sort_by { |player| player.inventories.count }
+    #@players = @item.players.sort{ |x,y| x.inventories.count <=> y.inventories.count }
   end
 
 end
