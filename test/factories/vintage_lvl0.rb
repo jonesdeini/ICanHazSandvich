c0c0c74f657327b54be300e7f7383d33605170d3
jonesdeini <jonesdeini@gmail.com>
2012-11-21 18:12:39 -0500

    vintage lvl0 basic test

---
+++ test/factories/vintage_lvl0.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :vintage_lvl0, class: VintageLvl0 do
+    name "vintage_lvl0"
+    level 0
+    quality 3
+  end
+end
---
+++ test/models/vintage_lvl0_test.rb
@@ -0,0 +1,18 @@
+
+require 'minitest_helper'
+
+describe VintageLvl0 do
+
+  before do
+    @vintage_lvl0 = create :vintage_lvl0
+    file = File.open("./test/vintage_lvl0_bp.json")
+    @backpack = JSON.parse(file.read)
+  end
+
+  it "must be valid" do
+    VintageLvl0.count.must_equal 1
+    # NOTE there is one vintage lvl 0 item in lvl0_bp
+    @vintage_lvl0.search(@backpack).must_equal 1
+  end
+
+end
