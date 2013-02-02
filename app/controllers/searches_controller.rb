a41f46f3691417fffbc415e86cebbaf734e41b60
jonesdeini <jonesdeini@gmail.com>
2013-02-02 01:05:54 -0500

    working start of front end for search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,24 +1,26 @@
 class SearchesController < ApplicationController
 
   def create
-
-  end
-
-  def index
-
+    params[:clans].each do |clan|
+      klass = "#{clan}Scraper".constantize
+      # thread.new { klass.new }
+      Delayed::Job.enqueue(ScraperJob.new(klass))
+    end
   end
 
   def new
-  @clans = ["Xxl",
-            "Sourceop",
-            "Drunkenbombers",
-            "Hyperiongaming",
-            "Intoxgaming",
-            "Lotus"]
+    @clans = ["Xxl",
+              "Sourceop",
+              "Drunkenbombers",
+              "Hyperiongaming",
+              "Intoxgaming",
+              "Lotus"]
   end
 
-  def show
+end
 
+class ScraperJob < Struct.new(:klass)
+  def perform
+    klass.new
   end
-
 end
